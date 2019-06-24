2df8ac40b03cfed99424c6f1c417d04a2f55dd88
yuceltkts <49358951+yuceltkts@users.noreply.github.com>
2019-06-25 00:31:39 +0300

    Add files via upload

---
+++ ProjectContext.cs
@@ -0,0 +1,37 @@
+﻿using MyDataBase.DAL.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataBase.DAL.ORM.Context
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext()
+        {
+            Database.Connection.ConnectionString = "Server = 317.database.windows.net;Database = MyDataBase; uid=sa;pwd =1234";
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Kisiler> Kisilers { get; set; }
+       // public DbSet<Order> Orders { get; set; }
+    }   //public DbSet<Customer> Customers { get; set; }
+       // public DbSet <Shipper> Shippers { get; set; }
+       // public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    
+}
+
+
